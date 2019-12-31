49f591515ce1a0b4f129d984bd84ce1367e718a5
jpotor <jinyong0425@naver.com>
2019-12-31 15:20:26 +0900

    GET vs POST

---
+++ GET_POST/post_get.php
@@ -0,0 +1,14 @@
+<?php
+	#url필드 이용하기
+	echo "GET : ";
+	print_r($_GET);
+	echo "<br>";
+
+	echo "REQUEST : ";
+	print_r($_REQUEST);
+	echo "<br>";
+
+	echo "POST : ";
+	print_r($_POST);
+	echo "<br>";
+?>
